346841b6f5ed892c57774de853b403e8d83b41f5
Edison Junior <100057143+Edisonepj@users.noreply.github.com>
2023-11-05 20:59:50 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,46 +1,37 @@
-function isValidForm() {
-    const nome = document.getElementById("nome").value;
-    const setor = document.getElementById("setor").value;
-    const email = document.getElementById("email").value;
-    const telefone = document.getElementById("telefone").value;
-    const descricao = document.getElementById("descricao").value;
+document.addEventListener("DOMContentLoaded", function() {
+    const form = document.getElementById("registrationForm");
+    form.addEventListener("submit", function(event) {
+        event.preventDefault();
+        
+        const nome = document.getElementById("nome").value;
+        const setor = document.getElementById("setor").value;
+        const email = document.getElementById("email").value;
+        const telefone = document.getElementById("telefone").value;
+        const descricao = document.getElementById("descricao").value;
 
-    if (!isValidEmail(email)) {
-        alert("Por favor, insira um endereço de e-mail válido.");
-        return false;
+        if (!isValidEmail(email)) {
+            alert("Por favor, insira um endereço de e-mail válido.");
+            return;
+        }
+
+        if (!isValidPhone(telefone)) {
+            alert("Por favor, insira um número de telefone válido com DDD e 9 dígitos.");
+            return;
+        }
+
+        alert("Formulário enviado com sucesso!");
+    });
+
+    function isValidEmail(email) {
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        return emailRegex.test(email);
     }
 
-    if (!isValidPhone(telefone)) {
-        alert("Por favor, insira um número de telefone válido com DDD e 9 dígitos.");
-        return false;
-    }
+    function isValidPhone(telefone) {
+        // Remove todos os caracteres não numéricos do valor do campo.
+        const phoneNumber = telefone.replace(/\D/g, '');
 
-    alert("Formulário enviado com sucesso!");
-    return true;
-}
-
-function isValidEmail(email) {
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return emailRegex.test(email);
-}
-
-function isValidPhone(telefone) {
-    // Validação para um número de telefone com DDD e 9 dígitos (xx xxxxx-xxxx).
-    const phoneRegex = /^(\d{2})\s\d{5}-\d{4}$/;
-    return phoneRegex.test(telefone);
-}
-function formatPhoneNumber(input) {
-    // Remove todos os caracteres não numéricos do valor do campo.
-    const phoneNumber = input.value.replace(/\D/g, '');
-
-    // Formata o número de telefone com parenteses para o DDD e um traço.
-    if (phoneNumber.length >= 2) {
-        input.value = `(${phoneNumber.substring(0, 2)}`;
+        // Verifica se o número de telefone tem 11 dígitos, incluindo DDD.
+        return phoneNumber.length === 11;
     }
-    if (phoneNumber.length >= 7) {
-        input.value = `(${phoneNumber.substring(0, 2)}) ${phoneNumber.substring(2, 7)}-`;
-    }
-    if (phoneNumber.length >= 11) {
-        input.value = `(${phoneNumber.substring(0, 2)}) ${phoneNumber.substring(2, 7)}-${phoneNumber.substring(7, 11)}`;
-    }
-}
+});
