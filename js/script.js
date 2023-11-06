9e2f42eb82aaf5e55fcd072387a0ccc46101f49d
Edison Junior <100057143+Edisonepj@users.noreply.github.com>
2023-11-05 21:04:24 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,37 +1,31 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const form = document.getElementById("registrationForm");
-    form.addEventListener("submit", function(event) {
-        event.preventDefault();
-        
-        const nome = document.getElementById("nome").value;
-        const setor = document.getElementById("setor").value;
-        const email = document.getElementById("email").value;
-        const telefone = document.getElementById("telefone").value;
-        const descricao = document.getElementById("descricao").value;
+function isValidForm() {
+    const nome = document.getElementById("nome").value;
+    const setor = document.getElementById("setor").value;
+    const email = document.getElementById("email").value;
+    const telefone = document.getElementById("telefone").value;
+    const descricao = document.getElementById("descricao").value;
 
-        if (!isValidEmail(email)) {
-            alert("Por favor, insira um endereço de e-mail válido.");
-            return;
-        }
-
-        if (!isValidPhone(telefone)) {
-            alert("Por favor, insira um número de telefone válido com DDD e 9 dígitos.");
-            return;
-        }
-
-        alert("Formulário enviado com sucesso!");
-    });
-
-    function isValidEmail(email) {
-        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-        return emailRegex.test(email);
+    if (!isValidEmail(email)) {
+        alert("Por favor, insira um endereço de e-mail válido.");
+        return false;
     }
 
-    function isValidPhone(telefone) {
-        // Remove todos os caracteres não numéricos do valor do campo.
-        const phoneNumber = telefone.replace(/\D/g, '');
-
-        // Verifica se o número de telefone tem 11 dígitos, incluindo DDD.
-        return phoneNumber.length === 11;
+    if (!isValidPhone(telefone)) {
+        alert("Por favor, insira um número de telefone válido no formato (xx) xxxxx-xxxx.");
+        return false;
     }
-});
+
+    alert("Formulário enviado com sucesso!");
+    return true;
+}
+
+function isValidEmail(email) {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+}
+
+function isValidPhone(telefone) {
+    // Validação para um número de telefone no formato (xx) xxxxx-xxxx.
+    const phoneRegex = /^\(\d{2}\) \d{5}-\d{4}$/;
+    return phoneRegex.test(telefone);
+}
