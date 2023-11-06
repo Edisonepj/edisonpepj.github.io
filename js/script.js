f534b87f94b0fb8275ed083f6737bd880273ba98
Edison Junior <100057143+Edisonepj@users.noreply.github.com>
2023-11-05 22:16:52 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,59 +1,48 @@
 document.addEventListener("DOMContentLoaded", function() {
+    // Importe as funções necessárias do SDK do Firebase
+    import { initializeApp } from "firebase/app";
+    import { getAnalytics } from "firebase/analytics";
+
+    // Sua configuração do Firebase para a web
+    const firebaseConfig = {
+      apiKey: "AIzaSyCwfiRk9IlJeaz5GhLHbvm1f9qrNq-5T3Q",
+      authDomain: "pi22023.firebaseapp.com",
+      projectId: "pi22023",
+      storageBucket: "pi22023.appspot.com",
+      messagingSenderId: "262575922279",
+      appId: "1:262575922279:web:6425b7b92efb5fa14908c2",
+      measurementId: "G-ZJGMCSW9F8" // Opcional se você tiver um ID de medição
+    };
+
+    // Inicialize o Firebase
+    const app = initializeApp(firebaseConfig);
+    const analytics = getAnalytics(app);
+
     const form = document.getElementById("registrationForm");
+
     form.addEventListener("submit", function(event) {
         event.preventDefault();
-        
+
+        // Validar os campos aqui
         const nome = document.getElementById("nome").value;
         const setor = document.getElementById("setor").value;
         const email = document.getElementById("email").value;
         const telefone = document.getElementById("telefone").value;
         const descricao = document.getElementById("descricao").value;
 
+        // Exemplo de validação simples do campo de e-mail
         if (!isValidEmail(email)) {
             alert("Por favor, insira um endereço de e-mail válido.");
-            return false;
+            return;
         }
 
-        if (!isValidPhone(telefone)) {
-            alert("Por favor, insira um número de telefone válido no formato (xx) xxxxx-xxxx.");
-            return false;
-        }
-
+        // Enviar os dados para o servidor ou fazer outras ações aqui
         alert("Formulário enviado com sucesso!");
-        return true;
     });
 
+    // Função de validação de e-mail simples
     function isValidEmail(email) {
         const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
         return emailRegex.test(email);
     }
-
-    function isValidPhone(telefone) {
-        const phoneRegex = /^\(\d{2}\) \d{5}-\d{4}$/;
-        return phoneRegex.test(telefone);
-    }
-
-    const telefoneInput = document.getElementById("telefone");
-
-    telefoneInput.addEventListener("input", function() {
-        let input = this;
-        let inputValue = input.value.replace(/\D/g, ''); // Remove caracteres não numéricos
-
-        if (inputValue.length > 11) {
-            inputValue = inputValue.slice(0, 11);
-        }
-
-        let formattedValue = "(";
-        
-        for (let i = 0; i < inputValue.length; i++) {
-            if (i === 2) {
-                formattedValue += ") ";
-            } else if (i === 7) {
-                formattedValue += "-";
-            }
-            formattedValue += inputValue[i];
-        }
-
-        input.value = formattedValue;
-    });
 });
