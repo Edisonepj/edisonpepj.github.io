979392440c7f21f20f180c01d22b9aa0d5ba6996
Edison Junior <100057143+Edisonepj@users.noreply.github.com>
2023-11-06 00:09:50 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,26 +1,30 @@
-document.addEventListener("DOMContentLoaded", function() {
-    // Importe as funções necessárias do SDK do Firebase
-    import { initializeApp } from "firebase/app";
-    import { getAnalytics } from "firebase/analytics";
-    import { getDatabase, ref, set, get } from "firebase/database";
+// Importe as funções necessárias do SDK do Firebase
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getDatabase, ref } from "firebase/database";
 
-    // Sua configuração do Firebase para a web
-    const firebaseConfig = {
-      apiKey: "AIzaSyCwfiRk9IlJeaz5GhLHbvm1f9qrNq-5T3Q",
-      authDomain: "pi22023.firebaseapp.com",
-      projectId: "pi22023",
-      storageBucket: "pi22023.appspot.com",
-      messagingSenderId: "262575922279",
-      appId: "1:262575922279:web:6425b7b92efb5fa14908c2",
-      measurementId: "G-ZJGMCSW9F8"
-    };
+// Configuração do Firebase da sua aplicação web
+const firebaseConfig = {
+  apiKey: "AIzaSyCwfiRk9IlJeaz5GhLHbvm1f9qrNq-5T3Q",
+  authDomain: "pi22023.firebaseapp.com",
+  databaseURL: "https://pi22023-default-rtdb.firebaseio.com",
+  projectId: "pi22023",
+  storageBucket: "pi22023.appspot.com",
+  messagingSenderId: "262575922279",
+  appId: "1:262575922279:web:6425b7b92efb5fa14908c2",
+  measurementId: "G-ZJGMCSW9F8"
+};
 
-    // Inicialize o Firebase
-    const app = initializeApp(firebaseConfig);
-    const analytics = getAnalytics(app);
+// Inicialize o Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
-    // Inicialize o Firebase Realtime Database
-    const db = getDatabase(app);
+// Use o Firebase Realtime Database
+const db = getDatabase(app);
+const dadosEmpresaRef = ref(db, 'empresas');
+
+// Agora você pode interagir com o Firebase Realtime Database usando o Firebase SDK no ambiente do navegador.
+
 
     // Exemplo de gravação de dados
     const dadosEmpresaRef = ref(db, 'empresas');
